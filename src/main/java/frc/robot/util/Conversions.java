b0b648319759432b4cd07f7999975b0ddb8e0525
Devin34 <halld35n@gmail.com>
2022-02-19 20:53:36 -0600

    feat(Conversions.java): added a class that converts measurments
    Ideally, we should stay consistent with our measurements and hopefully this will make it easier

---
+++ src/main/java/frc/robot/util/Conversions.java
@@ -0,0 +1,14 @@
+package frc.robot.util;
+
+public class Conversions {
+    
+    public double millimetersToMeters(double millimeters) {
+        return millimeters * 1000;
+    }
+
+    public double metersToMillimeters(double meters) {
+        return meters / 1000;
+    }
+
+
+}
